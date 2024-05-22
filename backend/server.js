ac6a24cc77b80ac611a5d3cc5da0ddc835466268
Ambar Soni <ambar.soni.che21@itbhu.ac.in>
2024-05-22 07:15:15 +0530

    socket completed

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,11 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
-
+import {app, server} from "./socket/socket.js"
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { Socket } from "socket.io";
 
-const app = express();
+
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -27,7 +28,7 @@ app.use("/api/users", userRoutes);
 // });
 
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
 
     connectToMongoDB();
     console.log(`Serer is running on ${PORT}`)
--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,16 +1,30 @@
 import React from 'react'
+import { useAuthContext } from "../../context/AuthContext.jsx";
+// import { extractTime } from "../../utils/extractTime";
+import useConversation from "../../zustand/useConversation.js";
+import { extractTime } from '../../utils/extractTime.js';
 
- const Message = () => {
+ const Message = ({message}) => {
+  const { authUser } = useAuthContext();
+	const { selectedConversation } = useConversation();
+  const fromMe = message.senderId === authUser._id;
+  const formattedTime = extractTime(message.createdAt);
+  const chatClassName = fromMe ? "chat-end" : "chat-start";
+	const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
+	const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+  const shakeClass = message.shouldShake ? "shake" : "";
   return (
-    <div className='chat chat-end'>
+    <div className={`chat ${chatClassName}`}>
         <div className='chat-image avatar'>
             <div className='w-10 rounded-full'>
-                <img src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FIMG_Academy&psig=AOvVaw3brrDJ411W36TlDrsRgaBL&ust=1711722608246000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMCm8-yVl4UDFQAAAAAdAAAAABAE' />
+                <img src= {profilePic} />
             </div>
 
         </div>
-        <div className = {'chat-bubble text-white bg-blue-500'}>Hi</div>
-        <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>12:42</div>
+        <div className = {`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-2`}>{message.message}</div>
+        <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>
+          {formattedTime}
+        </div>
     </div>
   )
 }
